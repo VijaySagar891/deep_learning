dbf56ee328ecaa31c97ec35ec932cb2d29ede313
Vijay <vijay.sagar891@gmail.com>
2019-05-25 10:13:01 -0700

    Playing around with datatypes

---
+++ keras_datatypes.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+x = np.array(123)
+print x
+print x.ndim
+print x.shape
+
+y = np.array([12, 34, 45])
+print y
+print y.ndim
+print y.shape
+
+z = np.array([
+                [1, 2, 3, 4],
+                [5, 6, 7, 8]])
+print z
+print z.ndim
+print z.shape
